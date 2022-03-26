1586d5565756b366b14820ac678b1d1849f5ae32
Filip Stachura <filip@appsilondatascience.com>
2022-03-26 19:25:38 +0100

    Example of pure HTLM Table.

--- examples/html_elements/app.R
+++ examples/html_elements/app.R
@@ -15,7 +15,16 @@ shinyApp(
     Label("test"),
     Pre("test"),
     OL(tags$li("test")),
-    UL(tags$li("test"))
+    UL(tags$li("test")),
+    HTMLTable(
+      tags$thead(tags$tr(tags$th("Project"), tags$th("Stack"), tags$th("Contributors"))),
+      tags$tbody(
+        tags$tr(tags$td("Blueprint"), tags$td("JS React"), tags$td("268")),
+        tags$tr(tags$td("TS"), tags$td("JSX"), tags$td("68")),
+        tags$tr(tags$td("shiny.blueprint"), tags$td("R JS"), tags$td("2"))
+      ),
+      tags$tfoot(tags$tr(tags$td("Total", colspan = 2), tags$td("1508")))
+    )
   ),
   server = function(input, output) {}
 )
