ef5d31c2391b1dab56b625ef727c75e7882c9f01
Kamil Zyla <kamil@appsilon.com>
2022-06-06 16:49:40 +0200

    docs: Organize components.R and documentation

--- R/components.R
+++ R/components.R
@@ -28,81 +28,14 @@ input <- function(name, defaultValue) {
 
 button <- function(name) {
   function(inputId, ...) shiny.react::reactElement(
-    module = "@/appsilon.blueprint", 
+    module = "@/appsilon.blueprint",
     name = name,
     props = shiny.react::asProps(inputId = inputId, ...),
     deps = blueprintDependency()
   )
 }
 
-#' @export
-ProgressBar <- component("ProgressBar")
-
-#' @export
-Callout <- component("Callout")
-
-#' @export
-Card <- component("Card")
-
-#' @export
-Pre <- component("Pre")
-
-#' @export
-H1 <- component("H1")
-
-#' @export
-H2 <- component("H2")
-
-#' @export
-H3 <- component("H3")
-
-#' @export
-H4 <- component("H4")
-
-#' @export
-H5 <- component("H5")
-
-#' @export
-H6 <- component("H6")
-
-#' @export
-Blockquote <- component("Blockquote")
-
-#' @export
-Code <- component("Code")
-
-#' @export
-Label <- component("Label")
-
-#' @export
-OL <- component("OL")
-
-#' @export
-UL <- component("UL")
-
-#' @export
-HTMLTable <- component("HTMLTable")
-
-#' @export
-Icon <- component("Icon")
-
-#' @export
-Divider <- component("Divider")
-
-#' @export
-Collapse <- component("Collapse")
-
-#' @export
-Switch <- component("Switch")
-
-#' @export
-Switch.shinyInput <- input("Switch", FALSE)
-
-#' @export
-Spinner <- component("Spinner")
-
-#' @export
-ButtonGroup <- component("ButtonGroup")
+# TODO(porting): Breadcrumbs
 
 #' @export
 Button <- component("Button")
@@ -111,31 +44,197 @@ Button <- component("Button")
 Button.shinyInput <- button("Button")
 
 #' @export
-EditableText.shinyInput <- input("EditableText", "")
+ButtonGroup <- component("ButtonGroup")
 
 #' @export
-MenuDivider <- component("MenuDivider")
+Callout <- component("Callout")
 
+#' @export
+Card <- component("Card")
+
+#' @export
+Collapse <- component("Collapse")
+
+# TODO(porting): Collapsible list
+
+#' @export
+Divider <- component("Divider")
+
+# TODO: Add EditableText plain component
+
+#' @export
+EditableText.shinyInput <- input("EditableText", "")
+
+#' HTML elements
+#' @example inst/examples/components/html_elements.R
+#' @name html_elements
+NULL
+
+#' @rdname html_elements
+#' @export
+H1 <- component("H1")
+
+#' @rdname html_elements
+#' @export
+H2 <- component("H2")
+
+#' @rdname html_elements
+#' @export
+H3 <- component("H3")
+
+#' @rdname html_elements
+#' @export
+H4 <- component("H4")
+
+#' @rdname html_elements
+#' @export
+H5 <- component("H5")
+
+#' @rdname html_elements
+#' @export
+H6 <- component("H6")
+
+#' @rdname html_elements
+#' @export
+Blockquote <- component("Blockquote")
+
+#' @rdname html_elements
+#' @export
+Code <- component("Code")
+
+#' @rdname html_elements
+#' @export
+Label <- component("Label")
+
+#' @rdname html_elements
+#' @export
+Pre <- component("Pre")
+
+#' @rdname html_elements
+#' @export
+OL <- component("OL")
+
+#' @rdname html_elements
+#' @export
+UL <- component("UL")
+
+#' @rdname html_elements
+#' @export
+HTMLTable <- component("HTMLTable")
+
+# TODO(porting): HotkeysTarget2
+
+#' Icon
+#' @example inst/examples/components/Icon.R
+#' @export
+Icon <- component("Icon")
+
+#' Menu
+#' @example inst/examples/components/Menu.R
 #' @export
 Menu <- component("Menu")
 
+#' @rdname Menu
 #' @export
 MenuItem <- component("MenuItem")
 
+#' @rdname Menu
+#' @export
+MenuDivider <- component("MenuDivider")
+
+#' Navbar
+#' @example inst/examples/components/Navbar.R
 #' @export
 Navbar <- component("Navbar")
 
+#' @rdname Navbar
 #' @export
 NavbarGroup <- component("NavbarGroup")
 
+#' @rdname Navbar
 #' @export
 NavbarHeading <- component("NavbarHeading")
 
+#' @rdname Navbar
 #' @export
 NavbarDivider <- component("NavbarDivider")
+
+#' Non-ideal state
+#' @example inst/examples/components/NonIdealState.R
+#' @export
+NonIdealState <- component("NonIdealState")
+
+# TODO(porting): Overflow list
+
+# TODO(porting): Panel stack (v2)
+
+#' @export
+ProgressBar <- component("ProgressBar")
+
+# TODO(porting): Resize sensor
+
+# TODO(porting): Skeleton
+
+#' @export
+Spinner <- component("Spinner")
+
+# TODO(porting): Tabs
+
+# TODO(porting): Tag
+
+# TODO(porting): Text
+
+# TODO(porting): Tree
+
+# TODO(porting): Form group
+
+# TODO(porting): Control group
+
+# TODO(porting): Label
+
+# TODO(porting): Checkbox
+
+# TODO(porting): Radio
+
+# TODO(porting): HTML select
+
+# TODO(porting): Slider
+
+#' Switch
+#' @example inst/examples/components/Switch.R
+#' @export
+Switch <- component("Switch")
+
+#' @rdname Switch
+#' @example inst/examples/components/Switch.shinyInput.R
+#' @export
+Switch.shinyInput <- input("Switch", FALSE)
+
+# TODO(porting): File input
+
+# TODO(porting): Numeric input
+
+# TODO(porting): Text inputs
+
+# TODO(porting): Tag input
+
+# TODO(porting): Overlay
+
+# TODO(porting): Portal
+
+# TODO(porting): Alert
+
+# TODO(porting): Context menu
+
+# TODO(porting): Dialogs
+
+# TODO(porting): Drawer
 
 #' @export
 Popover <- component("Popover")
 
-#' @export
-NonIdealState <- component("NonIdealState")
+# TODO(porting): Toast
+
+# TODO(porting): Tooltip
+
+# TODO(porting): HotkeysProvider
