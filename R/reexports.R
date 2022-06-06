a18dd0560cdb4ce397fcf2b2f3034dc1a0c5bc1c
Kamil Zyla <kamil@appsilon.com>
2022-06-06 14:31:52 +0200

    feat: Add shiny.react reexports

---
+++ R/reexports.R
@@ -0,0 +1,19 @@
+#' @importFrom shiny.react JS
+#' @export
+shiny.react::JS
+
+#' @importFrom shiny.react reactOutput
+#' @export
+shiny.react::reactOutput
+
+#' @importFrom shiny.react renderReact
+#' @export
+shiny.react::renderReact
+
+#' @importFrom shiny.react setInput
+#' @export
+shiny.react::setInput
+
+#' @importFrom shiny.react triggerEvent
+#' @export
+shiny.react::triggerEvent
