69d2856c3d17cfa2de1305def8cf4dfefa5d3f09
Filip Akkad <filip.akkad@gmail.com>
2022-06-08 15:13:52 +0200

    Styling improvements

--- inst/examples/components/Text.R
+++ inst/examples/components/Text.R
@@ -5,13 +5,13 @@ if (interactive()) shinyApp(
   ui = tagList(
     Text(
       "Lorem ipsum dolor sit amet,
-      consectetur adipiscing elit, 
+      consectetur adipiscing elit,
       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       Ut enim ad minim veniam,
       quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      Duis aute irure dolor in reprehenderit 
+      Duis aute irure dolor in reprehenderit
       in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Excepteur sint occaecat cupidatat non proident, 
+      Excepteur sint occaecat cupidatat non proident,
       sunt in culpa qui officia deserunt mollit anim id est laborum."
     )
   ),
